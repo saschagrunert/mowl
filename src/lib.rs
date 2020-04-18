edd3845142a6aa7040a0e6a845228dcff9b71aa4
Patrick Junger <pJunger@users.noreply.github.com>
2020-04-18 19:46:06 +0200

    Added fallback output to stderr instead of failing init. (#1)
    * Added stderr as fallback if terminal cannot be created.
    
    * Removed clippy warnings.
    
    * Changed formatting to use rustfmt.
    
    * Updated dependencies.

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,6 @@
 //! # }
 //! ```
 #![deny(missing_docs)]
-#[macro_use]
 extern crate failure;
 extern crate log;
 extern crate term;
@@ -18,7 +17,8 @@ extern crate time;
 
 use failure::Error;
 use log::{Level, LevelFilter, Log, Metadata, Record};
-use term::{color::*, stderr};
+use std::io::prelude::*;
+use term::{color::*, StderrTerminal};
 use time::now;
 
 /// Initializes the global logger with a specific `max_log_level`.
@@ -33,12 +33,17 @@ use time::now;
 /// warn!("A warning");
 /// info!("A info message");
 /// # }
-/// ```
+///```
+///
+/// # Errors
+///
+/// An error is returned if a logger has already been set.
 pub fn init_with_level(log_level: LevelFilter) -> Result<(), Error> {
     log::set_boxed_logger(Box::new(Logger {
         level: log_level,
         enable_colors: true,
-    })).map(|()| log::set_max_level(log_level))?;
+    }))
+    .map(|()| log::set_max_level(log_level))?;
     Ok(())
 }
 
@@ -56,11 +61,16 @@ pub fn init_with_level(log_level: LevelFilter) -> Result<(), Error> {
 /// info!("A info message");
 /// # }
 /// ```
+///
+/// # Errors
+///
+/// An error is returned if a logger has already been set.
 pub fn init_with_level_and_without_colors(log_level: LevelFilter) -> Result<(), Error> {
     log::set_boxed_logger(Box::new(Logger {
         level: log_level,
         enable_colors: false,
-    })).map(|()| log::set_max_level(log_level))?;
+    }))
+    .map(|()| log::set_max_level(log_level))?;
     Ok(())
 }
 
@@ -77,6 +87,10 @@ pub fn init_with_level_and_without_colors(log_level: LevelFilter) -> Result<(), 
 /// warn!("Warning");
 /// # }
 /// ```
+///
+/// # Errors
+///
+/// An error is returned if logger has already been initialized.
 pub fn init() -> Result<(), Error> {
     init_with_level(LevelFilter::Trace)
 }
@@ -106,7 +120,7 @@ impl Log for Logger {
 impl Logger {
     fn log_result(&self, record: &Record) -> Result<(), Error> {
         // We have to create a new terminal on each log because Send is not fulfilled
-        let mut t = stderr().ok_or_else(|| format_err!("Could not create terminal."))?;
+        let mut t = LogSink::new();
         if self.enable_colors {
             t.fg(BRIGHT_BLACK)?;
         }
@@ -114,7 +128,7 @@ impl Logger {
         if self.enable_colors {
             t.fg(BRIGHT_BLUE)?;
         }
-        write!(t, "[{}] ", record.module_path().unwrap_or("?"));
+        write!(t, "[{}] ", record.module_path().unwrap_or("?"))?;
         if self.enable_colors {
             match record.level() {
                 Level::Error => t.fg(BRIGHT_RED)?,
@@ -135,5 +149,75 @@ impl Logger {
     /// Disable coloring output
     pub fn disable_colors(&mut self) {
         self.enable_colors = false;
+    }
+}
+
+/// Different output implementations for the logger.
+enum LogSink {
+    /// Stderr Terminal as default
+    Terminal(Box<StderrTerminal>),
+    /// Stderr as fallback if a terminal cannot be instantiated
+    Fallback(std::io::Stderr),
+}
+
+impl LogSink {
+    fn new() -> Self {
+        if let Some(term) = term::stderr() {
+            Self::Terminal(term)
+        } else {
+            Self::Fallback(std::io::stderr())
+        }
+    }
+
+    fn fg(&mut self, color: Color) -> Result<(), Error> {
+        if let Self::Terminal(t) = self {
+            t.fg(color)?;
+        }
+        Ok(())
+    }
+
+    fn reset(&mut self) -> Result<(), Error> {
+        if let Self::Terminal(t) = self {
+            t.reset()?;
+        }
+        Ok(())
+    }
+}
+
+/// Implement Write for `LogSink` by forwarding to the underlying Writers
+impl std::io::Write for LogSink {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        match self {
+            Self::Terminal(t) => t.write(buf),
+            Self::Fallback(e) => e.write(buf),
+        }
+    }
+
+    fn write_vectored(&mut self, bufs: &[std::io::IoSlice<'_>]) -> std::io::Result<usize> {
+        match self {
+            Self::Terminal(t) => t.write_vectored(bufs),
+            Self::Fallback(e) => e.write_vectored(bufs),
+        }
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        match self {
+            Self::Terminal(t) => t.flush(),
+            Self::Fallback(e) => e.flush(),
+        }
+    }
+
+    fn write_all(&mut self, buf: &[u8]) -> std::io::Result<()> {
+        match self {
+            Self::Terminal(t) => t.write_all(buf),
+            Self::Fallback(e) => e.write_all(buf),
+        }
+    }
+
+    fn write_fmt(&mut self, args: std::fmt::Arguments<'_>) -> std::io::Result<()> {
+        match self {
+            Self::Terminal(t) => t.write_fmt(args),
+            Self::Fallback(e) => e.write_fmt(args),
+        }
     }
 }
